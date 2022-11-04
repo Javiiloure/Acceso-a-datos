56e472628cd8eaa4f66c2d719683f609ee1e48c0
= <=>
2022-11-04 09:53:00 +0100

    Finalizo tarea 6

--- Tarea6_Aleatorios_I/src/Main.java
+++ Tarea6_Aleatorios_I/src/Main.java
@@ -4,17 +4,14 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.FileWriter;
-import java.io.BufferedWriter;
 import java.io.IOException;
 
 public class Main {
 	static Scanner scan = new Scanner(System.in);
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-
+		
+		// Para probar el programa se ha escrito un archivo de texto con la palabra raton repetida varias veces
+		// Cuando se ejecute el programa sustituira la palabra raton por teclado en el archivo todas las veces que aparezca
 		String palabra_buscar = "raton";
 		String palabra_modificar = "teclado";
 		String ruta = "C:\\Users\\javie\\Desktop\\Acceso a Datos\\Tarea6_Aleatorios_I\\texto.txt";
@@ -33,7 +30,7 @@ public class Main {
 
 			// Declaramos un switch para elegir las disitintas opciones
 			switch (select) {
-			// Iniciamos JFileChooser en case 1 para seleccionar el archivo
+			// Lanzamos JFileChooser en case 1 para seleccionar el archivo
 			case "1":
 				JFileChooser fileChooser = new JFileChooser();
 				fileChooser.showOpenDialog(fileChooser);
@@ -55,11 +52,9 @@ public class Main {
 			case "3":
 				file = new File(ruta);
 				try {
-					//FileWriter fw = new FileWriter(file);
 					FileReader fr = new FileReader(file);
-					//BufferedWriter bw = new BufferedWriter(fw);
 					BufferedReader br = new BufferedReader(fr);
-					cambiarPalabra(br, palabra_buscar, palabra_modificar);
+					cambiarPalabra(br, file, palabra_buscar, palabra_modificar);
 				} catch (IOException e) {
 					System.out.println(e.getMessage());
 				}
@@ -75,18 +70,33 @@ public class Main {
 		}
 	}
 
-	public static void cambiarPalabra(BufferedReader br, String palabra_buscar, String palabra_modificar) throws IOException {
-
+	public static void cambiarPalabra(BufferedReader br, File file, String palabra_buscar, String palabra_modificar) throws IOException {
+		
+		// String linea para analizr cada linea
 		String linea = "";
-		int index;
-		System.out.println("aux");
+		//String contenido para guardar el contenido final que se escribira en el fichero
+		String contenido_final = "";
+		
+		// Bucle while que finaliza cuando no qeudan más lineas
 		while((linea = br.readLine()) != null) {
-			
-			System.out.println(linea);
+			// En cada linea se analiza si contiene la palabra que se esta buscando y si la contiene se sustituye todas las veces que aparezca 
+			// por la otra palara y se guarda esa nueva string en contenido_final
 			if(linea.contains(palabra_buscar)) {
-				linea.replace(palabra_buscar, palabra_modificar);
+				linea = linea.replaceAll(palabra_buscar, palabra_modificar);
+				contenido_final += linea + "\n";
+				System.out.println(linea);
+			//Si la linea no contiene la palabra simplemente se suma a contenido final
+			} else {
+				contenido_final += linea + "\n";
 				System.out.println(linea);
 			}
 		}
+		
+		// Una vez terminado el bucle, declaramos un FileWriter sin append a true para sustituir todo el contenido anterior del fichero
+		// por el nuevo contenido
+		FileWriter fw = new FileWriter(file);
+		fw.write(contenido_final);
+		fw.flush();
+		fw.close();
 	}
 }
